a8571012580490f6538e8f5edb230f7b460696f3
Hassanbhb <Hassan.benhajbouih@gmail.com>
2023-06-02 18:48:29 +0100

    added animations and delay plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,12 +14,19 @@ module.exports = {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
           '50%': { transform: 'rotate(3deg)' },
-        }
+        },
+				fadeIn: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
       },
 			animation: {
 				wiggle: 'wiggle 1s ease-in-out infinite',
-			}
+				fadeIn: 'fadeIn 1.6s ease-in-out forwards',
+			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require("tailwindcss-animation-delay")
+	],
 }
